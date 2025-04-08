cfeb16c9be7fcc9c44b76cf7fad7285e8213b5fc
Erick Duarte <eduarte1@asu.edu>
2025-04-08 12:26:38 -0700

    FLOR::Auto-commit::2025-04-08T12:26:18

--- easyframe_ocr.py
+++ easyframe_ocr.py
@@ -1,5 +1,5 @@
 import easyocr
-reader = easyocr.Reader(['en'], detection='DB', recognition = 'Transformer') # this needs to run only once to load the model into memory
-result = reader.readtext('/Users/erickduarte/git/segmentation/test_frames/The Slow Regard of Silent Things - Patrick Rothfuss (GoPro).mov/0000002070.jpg', detail = 0)
+reader = easyocr.Reader(['en']) # this needs to run only once to load the model into memory
+result = reader.readtext('/Users/erickduarte/git/segmentation/test_frames/The Plague Recording.mov/0000000315.jpg', detail = 0)
 
 print(result)
